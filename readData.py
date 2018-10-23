35f888d2f83a891d85cf1b168d0fdc2637318ead
Angelina <h1255513@gmail.com>
2018-10-22 23:31:16 -0400

    updated

--- readData.py
+++ readData.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 
 def import_data(directory):
+    #adding headers to the dataframe
     headers = ['Time', 'Voltage']
+    # reading data as panda dataframe
     data = pd.read_csv(directory, names=headers)
     return data
