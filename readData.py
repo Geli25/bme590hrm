0317039bed718e3b5a6d428d5790e6962e302bd3
Angelina <h1255513@gmail.com>
2018-10-23 00:00:24 -0400

    added exceptions

--- readData.py
+++ readData.py
@@ -1,9 +1,20 @@
 import pandas as pd
+import logging
 
 
 def import_data(directory):
-    # adding headers to the dataframe
-    headers = ['Time', 'Voltage']
-    # reading data as panda dataframe
-    data = pd.read_csv(directory, names=headers)
+    try:
+        # adding headers to the dataframe
+        headers = ['Time', 'Voltage']
+        # reading data as panda dataframe
+        data = pd.read_csv(directory, names=headers)
+    except FileNotFoundError:
+        logging.error("The file either does not exist or the file path is wrong, recheck")
+        return
+    except ValueError:
+        logging.error("A path is needed to run this function")
+        return
+    except NameError:
+        logging.error("The file path needs to be a string")
+        return
     return data
--- validateData.py
+++ validateData.py
@@ -1,23 +1,28 @@
 import pandas as pd
+import logging
 
 
 def validate(input_data):
     # Filter out all NaN value
-    data = input_data.dropna()
+    try:
+        data = input_data.dropna()
 
-    # Filter out all dataframe containing letters
-    df = data[pd.to_numeric(data.Voltage, errors='coerce').notnull()]
-    df2 = df[pd.to_numeric(df.Time, errors='coerce').notnull()]
+        # Filter out all dataframe containing letters
+        df = data[pd.to_numeric(data.Voltage, errors='coerce').notnull()]
+        df2 = df[pd.to_numeric(df.Time, errors='coerce').notnull()]
 
-    # Convert all dataframe into float
-    df2.loc["Time"] = df2.Time.astype(float)
-    df2.loc["Voltage"] = df2.Voltage.astype(float)
-    df2["Voltage"] = df2.Voltage.astype(float)
-    df2["Time"] = df2.Time.astype(float)
-    print(df2.dtypes)
+        # Convert all dataframe into float
+        df2.loc["Time"] = df2.Time.astype(float)
+        df2.loc["Voltage"] = df2.Voltage.astype(float)
+        df2["Voltage"] = df2.Voltage.astype(float)
+        df2["Time"] = df2.Time.astype(float)
+        print(df2.dtypes)
 
-    # Filter out dataframe outside of range
-    df3 = df2[df2['Voltage'] <= 300]
+        # Filter out dataframe outside of range
+        df3 = df2[df2['Voltage'] <= 300]
+
+    except ValueError:
+        logging.error("A panda dataframe is needed to run this method")
 
     print(df3)
     return df3
