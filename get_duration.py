4320d84cfb0a6fa4fc60250f15972625e68987fa
Angelina <h1255513@gmail.com>
2018-10-24 20:09:39 -0400

    pep8 mods and removed unneeded code

--- get_duration.py
+++ get_duration.py
@@ -1,13 +1,6 @@
-from readData import import_data
-from validateData import validate
-
-
 def get_duration(data):
     start_time = data.Time.min()
     end_time = data.Time.max()
     duration = end_time-start_time
     print(duration)
     return duration
-
-if __name__ == '__main__':
-    get_duration(validate(import_data("test_data/test_data32.csv")))
