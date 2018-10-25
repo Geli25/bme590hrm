9bb81108a2fe121742623e6eb297b5a7507a924e
Angelina <h1255513@gmail.com>
2018-10-25 18:38:26 -0400

    added test

--- calculate_mean_bpm.py
+++ calculate_mean_bpm.py
@@ -1,7 +1,3 @@
-from readData import import_data
-from validateData import validate
-from find_peak import find_peak
-from get_beat_times import get_beat_times
 import numpy as np
 import logging
 
@@ -12,18 +8,13 @@ def calculate_mean_bpm(data, beats, start_time=0, end_time=None):
     if end_time > data["Time"].iloc[-1]:
         logging.warning("The end time provided is over the duration of the ECG strip. Using default value instead")
         end_time = data["Time"].iloc[-1]
+    if start_time is None:
+        start_time = 0
     beats_in_range = np.where(np.logical_and(beats >= start_time, beats <= end_time))
     print(start_time, end_time)
     print(beats_in_range[0].size)
     bpm = (beats_in_range[0].size/(end_time-start_time))*60
-    print(end_time-start_time)
+    # print(end_time-start_time)
     rounded_bpm = round(bpm, 0)
     print(rounded_bpm)
     return rounded_bpm
-
-
-if __name__ == '__main__':
-    data = validate(import_data("test_data/test_data1.csv"))
-    peak = find_peak(data)
-    beat_times = get_beat_times(peak, data)
-    calculate_mean_bpm(data, beat_times, 1, 70)
--- test_hrm.py
+++ test_hrm.py
@@ -5,89 +5,106 @@ from find_min_max import find_min_max
 from find_peak import find_peak
 from get_duration import get_duration
 from get_beat_times import get_beat_times
+from calculate_mean_bpm import calculate_mean_bpm
 
 
-@pytest.mark.parametrize("a, expected", [
+@pytest.mark.parametrize("directory, expected", [
     ("test_data/test_data1.csv", 10000),
     ("test_data/test_data2.csv", 10000),
     ("test_data/test_data31.csv", 10000),
     ("test_data/test_data32.csv", 10000)
 ])
-def test_import_data(a, expected):
-    data = import_data(a)
+def test_import_data(directory, expected):
+    data = import_data(directory)
     # calculating the number of rows after loading to see if all are loaded
     count_row = data.shape[0]
     assert count_row == expected
 
 
-@pytest.mark.parametrize("a, expected", [
+@pytest.mark.parametrize("directory, expected", [
     ("test_data/test_data1.csv", 10000),
     ("test_data/test_data2.csv", 10000),
     ("test_data/test_data30.csv", 9998),
     ("test_data/test_data31.csv", 9996),
     ("test_data/test_data32.csv", 9601)
 ])
-def test_validate_data(a, expected):
-    data = import_data(a)
-    validated_data = validate(data)
+def test_validate_data(directory, expected):
+    data = import_data(directory)
+    validated_data = validate(directory)
     # calculating the number of rows after filtering to see if it is correct
     count_row = validated_data.shape[0]
     assert count_row == expected
 
 
-@pytest.mark.parametrize("a, expected", [
+@pytest.mark.parametrize("directory, expected", [
     ("test_data/test_data24.csv", (-1.73, 5.1175)),
     ("test_data/test_data27.csv", (-2.2825, 1.5025)),
     ("test_data/test_data30.csv", (-1.73, 5.1175)),
     ("test_data/test_data31.csv", (-0.19375, 0.7875)),
     ("test_data/test_data32.csv", (-375.0, 300.0))
 ])
-def test_find_min_max(a, expected):
-    data = import_data(a)
+def test_find_min_max(directory, expected):
+    data = import_data(directory)
     validated_data = validate(data)
     min_max = find_min_max(validated_data)
     assert min_max == expected
 
 
-@pytest.mark.parametrize("a,expected", [
+@pytest.mark.parametrize("directory,expected", [
     ("test_data/test_data17.csv", 19),
     ("test_data/test_data1.csv", 35),
     ("test_data/test_data30.csv", 37),
     ("test_data/test_data31.csv", 19),
     ("test_data/test_data32.csv", 19)
 ])
-def test_find_peak(a, expected):
-    data = import_data(a)
+def test_find_peak(directory, expected):
+    data = import_data(directory)
     validated_data = validate(data)
     peaks = find_peak(validated_data)
     assert peaks[1] == expected
 
 
-@pytest.mark.parametrize("a,expected", [
+@pytest.mark.parametrize("directory,expected", [
     ("test_data/test_data15.csv", 13.887),
     ("test_data/test_data1.csv", 27.775),
     ("test_data/test_data30.csv", 39.996),
     ("test_data/test_data31.csv", 13.887),
     ("test_data/test_data32.csv", 13.887)
 ])
-def test_get_duration(a, expected):
-    data = import_data(a)
+def test_get_duration(directory, expected):
+    data = import_data(directory)
     validated_data = validate(data)
     duration = get_duration(validated_data)
     assert duration == expected
 
 
-@pytest.mark.parametrize("a,expected", [
+@pytest.mark.parametrize("directory,expected", [
     ("test_data/test_data1.csv", [0.214, 27.772]),
     ("test_data/test_data20.csv", [0.043, 13.543]),
     ("test_data/test_data30.csv", [0.16, 39.436]),
     ("test_data/test_data31.csv", [0.042, 13.537]),
     ("test_data/test_data32.csv", [0.028, 13.003])
 ])
-def test_get_duration(a, expected):
-    data = import_data(a)
+def test_get_duration(directory, expected):
+    data = import_data(directory)
     validated_data = validate(data)
     peaks = find_peak(validated_data)
     beat_times = get_beat_times(peaks, validated_data)
     time_check = [beat_times[0], beat_times[-1]]
     assert time_check == expected
+
+
+@pytest.mark.parametrize("directory, start_time, end_time, expected", [
+    ("test_data/test_data15.csv", 1, 30, 154),
+    ("test_data/test_data25.csv", 2, 5, 60),
+    ("test_data/test_data30.csv", 0, 10, 54),
+    ("test_data/test_data31.csv", None, None, 82),
+    ("test_data/test_data32.csv", 10, 20, 77)
+])
+def test_calculate_mean_bpm(directory, start_time, end_time, expected):
+    data = import_data(directory)
+    validated_data = validate(data)
+    peaks = find_peak(validated_data)
+    beat_times = get_beat_times(peaks, validated_data)
+    mean_bpm = calculate_mean_bpm(validated_data, beat_times, start_time, end_time)
+    assert mean_bpm == expected
