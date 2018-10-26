7a6fc4d88d1fe59f88ce9c567b01468d0065e187
Angelina <h1255513@gmail.com>
2018-10-25 20:17:14 -0400

    added code for output json file

--- dictionary_output.py
+++ dictionary_output.py
@@ -1,2 +1,11 @@
+import json
+from dictionary import create_dictionary
 
 
+def output_json(directory):
+    metrics = create_dictionary(directory)
+    beats_list = list(metrics["beats"])
+    metrics["beats"] = beats_list
+    file_name = directory[10:][:-4]
+    with open(file_name+".json", "w") as output_file:
+        json.dump(metrics, output_file, indent=4)
